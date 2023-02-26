3883967c18e6f62fa7af1ef623d310fde8c64ec9
Ismael Laet <ismaellaet1@gmail.com>
2023-02-26 12:36:19 -0300

    test: Expense viewset

---
+++ api/tests/test_expense_viewset.py
@@ -0,0 +1,79 @@
+from datetime import datetime
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+
+from api.models import Expense
+from api.serializers import ExpenseSerializer
+
+
+class ExpenseViewSetTestCase(APITestCase):
+    def setUp(self):
+        self.expense1 = Expense.objects.create(
+            description="Expense 1", value="50.99", date=datetime.now().date()
+        )
+        self.expense2 = Expense.objects.create(
+            description="Expense 2", value="75.30", date=datetime.now().date()
+        )
+
+    def test_list_expenses(self):
+        response = self.client.get(reverse("expense-list"))
+        expenses = Expense.objects.all()
+        serializer = ExpenseSerializer(expenses, many=True)
+
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_valid_expense(self):
+        valid_data = {
+            "description": "Expense 3",
+            "value": "100.50",
+            "date": "2022-02-28",
+        }
+        response = self.client.post(path=reverse("expense-list"), data=valid_data)
+        expense = Expense.objects.get(id=response.data["id"])
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(expense.description, valid_data["description"])
+        self.assertEqual(str(expense.value), valid_data["value"])
+        self.assertEqual(str(expense.date), valid_data["date"])
+
+    def test_create_invalid_expense(self):
+        invalid_data = {
+            "description": "",
+            "value": "100.50",
+            "date": "2022-02-28",
+        }
+        response = self.client.post(path=reverse("expense-list"), data=invalid_data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_expense(self):
+        response = self.client.get(reverse("expense-detail", args=[self.expense1.id]))
+        expense = Expense.objects.get(id=self.expense1.id)
+        serializer = ExpenseSerializer(expense)
+
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_expense(self):
+        data = {
+            "description": "Updated Expense",
+            "value": "99.99",
+            "date": "2022-02-27",
+        }
+        url = reverse("expense-detail", args=[self.expense1.id])
+        response = self.client.put(path=url, data=data)
+        self.expense1.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.expense1.description, data["description"])
+        self.assertEqual(str(self.expense1.value), data["value"])
+        self.assertEqual(str(self.expense1.date), data["date"])
+
+    def test_delete_expense(self):
+        url = reverse("expense-detail", args=[self.expense2.id])
+        response = self.client.delete(path=url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Expense.objects.filter(id=self.expense2.id).exists())
