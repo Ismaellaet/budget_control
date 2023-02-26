73410e8a68d62d53339fd563afd9f99a78914044
Ismael Laet <ismaellaet1@gmail.com>
2023-02-26 10:40:59 -0300

    feat: Create income and expense models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Income(models.Model):
+    description = models.CharField(max_length=150)
+    value = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return f"{self.date} => {self.description}: R${self.value}"
+
+
+class Expense(models.Model):
+    description = models.CharField(max_length=150)
+    value = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return f"{self.date} => {self.description}: -R${self.value}"
