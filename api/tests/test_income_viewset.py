e4ce5fe9cae31cf1167353d3d5083d203a752d2e
Ismael Laet <ismaellaet1@gmail.com>
2023-02-26 12:25:05 -0300

    test: Income viewsets

---
+++ api/tests/test_income_viewset.py
@@ -0,0 +1,79 @@
+from datetime import datetime
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+
+from api.models import Income
+from api.serializers import IncomeSerializer
+
+
+class IncomeViewSetTestCase(APITestCase):
+    def setUp(self):
+        self.income1 = Income.objects.create(
+            description="Income 1", value="5000.00", date=datetime.now().date()
+        )
+        self.income2 = Income.objects.create(
+            description="Income 2", value="1000.00", date=datetime.now().date()
+        )
+
+    def test_list_incomes(self):
+        response = self.client.get(reverse("income-list"))
+        incomes = Income.objects.all()
+        serializer = IncomeSerializer(incomes, many=True)
+
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_valid_income(self):
+        valid_data = {
+            "description": "Income 3",
+            "value": "503.00",
+            "date": "2022-02-28",
+        }
+        response = self.client.post(path=reverse("income-list"), data=valid_data)
+        income = Income.objects.get(id=response.data["id"])
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(income.description, valid_data["description"])
+        self.assertEqual(str(income.value), valid_data["value"])
+        self.assertEqual(str(income.date), valid_data["date"])
+
+    def test_create_invalid_income(self):
+        invalid_payload = {
+            "description": "",
+            "value": "1000.00",
+            "date": "2022-02-15",
+        }
+        response = self.client.post(path=reverse("income-list"), data=invalid_payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_income(self):
+        response = self.client.get(reverse("income-detail", args=[self.income1.id]))
+        income = Income.objects.get(id=self.income1.id)
+        serializer = IncomeSerializer(income)
+
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_income(self):
+        data = {
+            "description": "Updated Income",
+            "value": "6000.00",
+            "date": "2022-03-01",
+        }
+        url = reverse("income-detail", args=[self.income1.id])
+        response = self.client.put(path=url, data=data)
+        self.income1.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.income1.description, data["description"])
+        self.assertEqual(str(self.income1.value), data["value"])
+        self.assertEqual(str(self.income1.date), data["date"])
+
+    def test_delete_income(self):
+        url = reverse("income-detail", args=[self.income1.id])
+        response = self.client.delete(path=url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Income.objects.filter(id=self.income1.id).exists())
