ff9e412b099eeffb0aa26a46d69e70c2d7db7ede
Ismael Laet <ismaellaet1@gmail.com>
2023-03-03 21:35:19 -0300

    feat:Add category field in expense

--- api/models.py
+++ api/models.py
@@ -11,9 +11,20 @@ class Income(models.Model):
 
 
 class Expense(models.Model):
+    CATEGORIES = [
+        ("food", "food"),
+        ("health", "health"),
+        ("housing", "housing"),
+        ("transportation", "transportation"),
+        ("education", "education"),
+        ("leisure", "leisure"),
+        ("unforeseen", "unforeseen"),
+        ("other", "other"),
+    ]
     description = models.CharField(max_length=150)
     value = models.DecimalField(max_digits=6, decimal_places=2)
     date = models.DateField()
+    category = models.CharField(max_length=50, choices=CATEGORIES, default="other")
 
     def __str__(self) -> str:
         return f"{self.date} => {self.description}: -R${self.value}"
