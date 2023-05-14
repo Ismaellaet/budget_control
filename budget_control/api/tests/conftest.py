28fe138f0a9cc256436bbefc812d78450da1414a
Ismael de Laet <ismaellaet1@gmail.com>
2023-05-14 19:56:50 -0300

    refactor: Update Incomes tests to use pytest

--- budget_control/api/tests/conftest.py
+++ budget_control/api/tests/conftest.py
@@ -4,10 +4,16 @@ from datetime import date
 from model_bakery import baker
 from rest_framework.test import APIClient
 
+
 @pytest.fixture()
 def api_client():
     client = APIClient()
     return client
+
+
+@pytest.fixture()
+def mock_incomes():
+    return baker.make("Income", 2, date=date(2023, 5, 14))
 
 
 @pytest.fixture()
--- budget_control/api/tests/test_income_viewset.py
+++ budget_control/api/tests/test_income_viewset.py
@@ -1,98 +1,79 @@
-from datetime import datetime
+import pytest
 
+from datetime import datetime, date
 from django.urls import reverse
-
 from model_bakery import baker
-from rest_framework.test import APITestCase
 from rest_framework import status
 
 from ...models import Income
-from ..serializers.incomes import IncomeSerializer
 
 
-class IncomeViewSetTestCase(APITestCase):
-    def setUp(self):
-        self.incomes = baker.make("Income", 2)
+@pytest.fixture()
+def income_data():
+    return {
+        "description": "Income Test",
+        "value": "100.50",
+        "date": "2023-05-14"
+    }
 
-    def test_list_incomes(self):
-        response = self.client.get(reverse("income-list"))
-        serializer = IncomeSerializer(self.incomes, many=True)
 
-        self.assertEqual(response.data, serializer.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+@pytest.mark.django_db
+class TestIncomeViewSet:
 
-    def test_create_valid_income(self):
-        valid_data = {
-            "description": "Income 3",
-            "value": "503.00",
-            "date": "2022-02-28",
-        }
-        response = self.client.post(path=reverse("income-list"), data=valid_data)
-        income = Income.objects.get(id=response.data["id"])
+    def test_list_incomes(self, api_client, mock_incomes):
+        response = api_client.get(reverse("income-list"))
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(income.description, valid_data["description"])
-        self.assertEqual(str(income.value), valid_data["value"])
-        self.assertEqual(str(income.date), valid_data["date"])
+        assert response.status_code == status.HTTP_200_OK
 
-    def test_create_invalid_income(self):
-        invalid_payload = {
-            "description": "",
-            "value": "1000.00",
-            "date": "2022-02-15",
-        }
-        response = self.client.post(path=reverse("income-list"), data=invalid_payload)
+    def test_create_valid_income(self, api_client, income_data):
+        response = api_client.post(path=reverse("income-list"), data=income_data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data["description"] == income_data["description"]
+        assert response.data["value"] == income_data["value"]
+        assert response.data["date"] == income_data["date"]
 
-    def test_retrieve_income(self):
-        income = self.incomes[0]
-        response = self.client.get(reverse("income-detail", args=[income.id]))
-        income = Income.objects.get(id=income.id)
-        serializer = IncomeSerializer(income)
+    def test_retrieve_income(self, api_client):
+        income = baker.make("Income")
+        response = api_client.get(reverse("income-detail", args=[income.id]))
 
-        self.assertEqual(response.data, serializer.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["description"] == income.description
+        assert response.data["value"] == str(income.value)
+        assert response.data["date"] == str(income.date)
 
-    def test_update_income(self):
-        income = self.incomes[0]
-        data = {
-            "description": "Updated Income",
-            "value": "6000.00",
-            "date": "2022-03-01",
-        }
-
+    def test_update_income(self, api_client):
+        income = baker.make("Income")
         url = reverse("income-detail", args=[income.id])
-        response = self.client.put(path=url, data=data)
+        response = api_client.put(path=url, data={
+            "description": "Updated Income",
+            "value": "99.99",
+            "date": "2022-02-27",
+        })
         income.refresh_from_db()
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(income.description, data["description"])
-        self.assertEqual(str(income.value), data["value"])
-        self.assertEqual(str(income.date), data["date"])
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["description"] == income.description
+        assert response.data["value"] == str(income.value)
+        assert response.data["date"] == str(income.date)
 
-    def test_delete_income(self):
-        income = self.incomes[0]
+    def test_delete_income(self, api_client):
+        income = baker.make("Income")
         url = reverse("income-detail", args=[income.id])
-        response = self.client.delete(path=url)
+        response = api_client.delete(path=url)
 
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertFalse(Income.objects.filter(id=income.id).exists())
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert not Income.objects.filter(id=income.id).exists()
 
+    def test_get_incomes_by_year_month(self, api_client, mock_incomes):
+        year, month = 2022, 3
+        baker.make("Income", 2, date=date(year, month, 10))
+        url = reverse("income-by-year-month", args=[year, f"0{month}"])
+        response = api_client.get(url)
 
-class IncomeByYearMonthTestCase(APITestCase):
-    def setUp(self):
-        self.date = datetime.now()
-        self.year = self.date.year
-        self.month = f"{self.date:%m}"
-        self.incomes = baker.make("Income", 2, date=self.date)
-
-    def test_must_get_incomes_by_year_month(self):
-        url = reverse("income-by-year-month", args=[self.year, self.month])
-        response = self.client.get(path=url)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        for income in response.data:
+        assert response.status_code == status.HTTP_200_OK
+        for income in response.json():
             income_date = datetime.strptime(income["date"], "%Y-%m-%d")
-            self.assertEqual(income_date.year, self.year)
-            self.assertEqual(f"{income_date.month:02d}", self.month)
+            assert income not in mock_incomes
+            assert income_date.year == year
+            assert income_date.month == month
